b4e63c51d7dc36b1958ca78b4c0cb8d59a6836cb
kepoly <spadamatthew@gmail.com>
2020-09-28 18:59:19 -0400

    updates to ledger

--- packages/ledger/src/ledger.ts
+++ packages/ledger/src/ledger.ts
@@ -1,15 +1,9 @@
 import Transport from '@ledgerhq/hw-transport'
 import * as BIPPath from 'bip32-path'
 
-type AddressResult = {
-    publicKey: string,
-    address: string,
-    chainCode?: string
-  }
+const SW_OK = 0x9000;
+const SW_KEEP_ALIVE = 0x6e02;
 
-type ConfigResult = {
-    version: string
-}
 
 /**
  * Casinocoin Ledger API
@@ -19,24 +13,23 @@ type ConfigResult = {
  * const csc = new CSC(transport);
  */
 export default class CSC {
-  transport: Transport
+  transport: Transport;
 
-  constructor(transport: Transport) {
-    this.transport = transport
+  constructor(transport: Transport, scrambleKey: string = "CSC") {
+    this.transport = transport;
     transport.decorateAppAPIMethods(
       this,
-      ['getAddress', 'signTransaction', 'getAppConfiguration'],
-      'w0w'
+      ['getAddress', 'getAppConfiguration'],
+      scrambleKey
     )
   }
 
   /**
    * get Casinocoin address for a given BIP 32 path.
-   *
    * @param path a path in BIP 32 format
    * @param display optionally enable or not the display
    * @param chainCode optionally enable or not the chainCode request
-   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
+   * @param ed25519 optionally enable || ! the ed25519 curve (secp256k1 default)
    * @return an object with a publicKey, address and (optionally) chainCode
    * @example
    * const result = await csc.getAddress("44'/144'/0'/0/0");
@@ -52,116 +45,53 @@ export default class CSC {
     address: string,
     chainCode?: string
   }> {
-    const bipPath = BIPPath.fromString(path).toPathArray()
-    const curveMask = ed25519 ? 0x80 : 0x40
+    console.log('gg');
+    const bipPath = BIPPath.fromString(path).toPathArray();
+    const curveMask = ed25519 ? 0x80 : 0x40;
+    console.log('gg1');
 
-    const cla = 0xe0
-    const ins = 0x02
-    const p1 = display ? 0x01 : 0x00
-    const p2 = curveMask | (chainCode ? 0x01 : 0x00)
-    const data = Buffer.alloc(1 + bipPath.length * 4)
+    const cla = 0xe0;
+    const ins = 0x02;
+    const p1 = display ? 0x01 : 0x00;
+    const p2 = curveMask | (chainCode ? 0x01 : 0x00);
+    const data = Buffer.alloc(1 + bipPath.length * 4);
 
-    data.writeInt8(bipPath.length, 0)
+    data.writeInt8(bipPath.length, 0);
     bipPath.forEach((segment, index) => {
-      data.writeUInt32BE(segment, 1 + index * 4)
-    })
+      data.writeUInt32BE(segment, 1 + index * 4);
+    });
+    console.log('gg2');
+    console.log(cla, ins, p1, p2, data);
+    // const response = this.transport.send(cla, ins, p1, p2, data, [0x9000]);
+    return this.transport.send(cla, ins, p1, p2, data, [SW_OK, SW_KEEP_ALIVE])
+        .then(response => {
+      console.log(response);
+      const result = {address: '', publicKey: '', chainCode: ''};
+      const publicKeyLength = response[0];
+      const addressLength = response[1 + publicKeyLength];
+      console.log('gg3');
 
-    const response = await this.transport.send(cla, ins, p1, p2, data)
+      result.publicKey = response.slice(1, 1 + publicKeyLength).toString('hex');
+      console.log('gg4');
 
-    const result: AddressResult = {address: '', publicKey: ''}
-    const publicKeyLength = response[0]
-    const addressLength = response[1 + publicKeyLength]
+      result.address = response
+          .slice(1 + publicKeyLength + 1,
+              1 + publicKeyLength + 1 + addressLength)
+          .toString('ascii');
+      console.log('gg5');
 
-    result.publicKey = response.slice(1, 1 + publicKeyLength).toString('hex')
-
-    result.address = response
-      .slice(1 + publicKeyLength + 1, 1 + publicKeyLength + 1 + addressLength)
-      .toString('ascii')
-
-    if (chainCode) {
-      result.chainCode = response
-        .slice(
-          1 + publicKeyLength + 1 + addressLength,
-          1 + publicKeyLength + 1 + addressLength + 32
-        )
-        .toString('hex')
-    }
-
-    return result
-  }
-
-  /**
-   * sign a Casinocoin transaction with a given BIP 32 path
-   *
-   * @param path a path in BIP 32 format
-   * @param rawTxHex a raw transaction hex string
-   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
-   * @return a signature as hex string
-   * @example
-   * const signature = await csc.signTransaction("44'/144'/0'/0/0", "12000022800000002400000002614000000001315D3468400000000000000C73210324E5F600B52BB3D9246D49C4AB1722BA7F32B7A3E4F9F2B8A1A28B9118CC36C48114F31B152151B6F42C1D61FE4139D34B424C8647D183142ECFC1831F6E979C6DA907E88B1CAD602DB59E2F");
-   */
-  async signTransaction(
-    path: string,
-    rawTxHex: string,
-    ed25519?: boolean
-  ): Promise<string> {
-    const bipPath = BIPPath.fromString(path).toPathArray()
-    const rawTx = new Buffer(rawTxHex, 'hex')
-    const curveMask = ed25519 ? 0x80 : 0x40
-
-    const apdus = []
-    let offset = 0
-
-    while (offset !== rawTx.length) {
-      const maxChunkSize = offset === 0 ? 150 - 1 - bipPath.length * 4 : 150
-      const chunkSize =
-        offset + maxChunkSize > rawTx.length
-          ? rawTx.length - offset
-          : maxChunkSize
-
-      const apdu = {
-        cla: 0xe0,
-        ins: 0x04,
-        p1: offset === 0 ? 0x00 : 0x80,
-        p2: curveMask,
-        data:
-          offset === 0
-            ? Buffer.alloc(1 + bipPath.length * 4 + chunkSize)
-            : Buffer.alloc(chunkSize)
+      if (chainCode) {
+        result.chainCode = response
+            .slice(
+                1 + publicKeyLength + 1 + addressLength,
+                1 + publicKeyLength + 1 + addressLength + 32
+            )
+            .toString('hex');
       }
-
-      if (offset === 0) {
-        apdu.data.writeInt8(bipPath.length, 0)
-        bipPath.forEach((segment, index) => {
-          apdu.data.writeUInt32BE(segment, 1 + index * 4)
-        })
-        rawTx.copy(
-          apdu.data,
-          1 + bipPath.length * 4,
-          offset,
-          offset + chunkSize
-        )
-      } else {
-        rawTx.copy(apdu.data, 0, offset, offset + chunkSize)
-      }
-
-      apdus.push(apdu)
-      offset += chunkSize
-    }
-
-    let response = Buffer.alloc(0)
-    for (const apdu of apdus) {
-      response = await this.transport.send(
-        apdu.cla,
-        apdu.ins,
-        apdu.p1,
-        apdu.p2,
-        apdu.data
-      )
-    }
-
-    // the last 2 bytes are status code from the hardware
-    return response.slice(0, response.length - 2).toString('hex')
+      return result
+    }).catch(e => {
+      console.log('ERROR: ' + e);
+    });
   }
 
   /**
@@ -178,8 +108,8 @@ export default class CSC {
   async getAppConfiguration(): Promise<{
     version: string
   }> {
-    const response = await this.transport.send(0xe0, 0x06, 0x00, 0x00)
-    const result:ConfigResult = { version: ''}
+    const response = await this.transport.send(0xe0, 0x06, 0x00, 0x00);
+    const result = {'version': ''};
     result.version = '' + response[1] + '.' + response[2] + '.' + response[3]
     return result
   }
