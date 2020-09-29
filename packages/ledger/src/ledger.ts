86fbb3f8372b92d7e2897e6285e016d5e30ed430
kepoly <spadamatthew@gmail.com>
2020-09-28 20:31:37 -0400

    trying to fix broken module after version updates

--- packages/ledger/src/ledger.ts
+++ packages/ledger/src/ledger.ts
@@ -1,7 +1,15 @@
 import Transport from '@ledgerhq/hw-transport'
 import * as BIPPath from 'bip32-path'
-const SW_OK = 0x9000
-const SW_KEEP_ALIVE = 0x6e02
+
+type AddressResult = {
+  publicKey: string,
+  address: string,
+  chainCode?: string
+}
+
+type ConfigResult = {
+  version: string
+}
 
 /**
  * Casinocoin Ledger API
@@ -13,31 +21,32 @@ const SW_KEEP_ALIVE = 0x6e02
 export default class CSC {
   transport: Transport
 
-  constructor(transport: Transport, scrambleKey: string = 'CSC') {
+  constructor(transport: Transport) {
     this.transport = transport
     transport.decorateAppAPIMethods(
-      this,
-      ['getAddress', 'getAppConfiguration'],
-      scrambleKey
+        this,
+        ['getAddress', 'signTransaction', 'getAppConfiguration'],
+        'w0w'
     )
   }
 
   /**
    * get Casinocoin address for a given BIP 32 path.
+   *
    * @param path a path in BIP 32 format
    * @param display optionally enable or not the display
    * @param chainCode optionally enable or not the chainCode request
-   * @param ed25519 optionally enable || ! the ed25519 curve (secp256k1 default)
+   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
    * @return an object with a publicKey, address and (optionally) chainCode
    * @example
    * const result = await csc.getAddress("44'/144'/0'/0/0");
    * const { publicKey, address } = result;
    */
   async getAddress(
-    path: string,
-    display?: boolean,
-    chainCode?: boolean,
-    ed25519?: boolean
+      path: string,
+      display?: boolean,
+      chainCode?: boolean,
+      ed25519?: boolean
   ): Promise<{
     publicKey: string,
     address: string,
@@ -56,32 +65,41 @@ export default class CSC {
     bipPath.forEach((segment, index) => {
       data.writeUInt32BE(segment, 1 + index * 4)
     })
-    console.log(cla, ins, p1, p2, data)
-    return this.transport.send(cla, ins, p1, p2, data, [SW_OK, SW_KEEP_ALIVE])
-        .then(response => {
-      const result = {address: '', publicKey: '', chainCode: ''}
-      const publicKeyLength = response[0]
-      const addressLength = response[1 + publicKeyLength]
-      result.publicKey = response.slice(1, 1 + publicKeyLength).toString('hex')
-      result.address = response
-          .slice(1 + publicKeyLength + 1,
-              1 + publicKeyLength + 1 + addressLength)
-          .toString('ascii')
 
-      if (chainCode) {
-        result.chainCode = response
-            .slice(
-                1 + publicKeyLength + 1 + addressLength,
-                1 + publicKeyLength + 1 + addressLength + 32
-            )
-            .toString('hex')
-      }
-      return result
-    }).catch(e => {
-      console.log('ERROR: ' + e)
-    })
+    const response = await this.transport.send(cla, ins, p1, p2, data)
+
+    const result: AddressResult = {address: '', publicKey: ''}
+    const publicKeyLength = response[0]
+    const addressLength = response[1 + publicKeyLength]
+
+    result.publicKey = response.slice(1, 1 + publicKeyLength).toString('hex')
+
+    result.address = response
+        .slice(1 + publicKeyLength + 1, 1 + publicKeyLength + 1 + addressLength)
+        .toString('ascii')
+
+    if (chainCode) {
+      result.chainCode = response
+          .slice(
+              1 + publicKeyLength + 1 + addressLength,
+              1 + publicKeyLength + 1 + addressLength + 32
+          )
+          .toString('hex')
+    }
+
+    return result
   }
 
+  /**
+   * sign a Casinocoin transaction with a given BIP 32 path
+   *
+   * @param path a path in BIP 32 format
+   * @param rawTxHex a raw transaction hex string
+   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
+   * @return a signature as hex string
+   * @example
+   * const signature = await csc.signTransaction("44'/144'/0'/0/0", "12000022800000002400000002614000000001315D3468400000000000000C73210324E5F600B52BB3D9246D49C4AB1722BA7F32B7A3E4F9F2B8A1A28B9118CC36C48114F31B152151B6F42C1D61FE4139D34B424C8647D183142ECFC1831F6E979C6DA907E88B1CAD602DB59E2F");
+   */
   async signTransaction(
       path: string,
       rawTxHex: string,
@@ -160,7 +178,7 @@ export default class CSC {
     version: string
   }> {
     const response = await this.transport.send(0xe0, 0x06, 0x00, 0x00)
-    const result = {'version': ''}
+    const result: ConfigResult = {version: ''}
     result.version = '' + response[1] + '.' + response[2] + '.' + response[3]
     return result
   }
