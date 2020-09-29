8b70563639915cdb9f36f99de0d156d10f91034d
kepoly <spadamatthew@gmail.com>
2020-09-28 21:38:28 -0400

    ts lint

--- packages/ledger/src/ledger.ts
+++ packages/ledger/src/ledger.ts
@@ -36,7 +36,8 @@ export default class CSC {
    * @param path a path in BIP 32 format
    * @param display optionally enable or not the display
    * @param chainCode optionally enable or not the chainCode request
-   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
+   * @param ed25519 optionally enable or not the ed25519 curve
+   * (secp256k1 is default)
    * @return an object with a publicKey, address and (optionally) chainCode
    * @example
    * const result = await csc.getAddress("44'/144'/0'/0/0");
@@ -95,29 +96,33 @@ export default class CSC {
    *
    * @param path a path in BIP 32 format
    * @param rawTxHex a raw transaction hex string
-   * @param ed25519 optionally enable or not the ed25519 curve (secp256k1 is default)
+   * @param ed25519 optionally enable or not the ed25519 curve
+   * (secp256k1 is default)
    * @return a signature as hex string
    * @example
-   * const signature = await csc.signTransaction("44'/144'/0'/0/0", "12000022800000002400000002614000000001315D3468400000000000000C73210324E5F600B52BB3D9246D49C4AB1722BA7F32B7A3E4F9F2B8A1A28B9118CC36C48114F31B152151B6F42C1D61FE4139D34B424C8647D183142ECFC1831F6E979C6DA907E88B1CAD602DB59E2F");
+   * const signature = await csc.signTransaction("44'/144'/0'/0/0",
+   * "12000022800000002400000002614000000001315D3468400000000000000
+   * C73210324E5F600B52BB3D9246D49C4AB1722BA7F32B7A3E4F9F2B8A1A28B9
+   * 118CC36C48114F31B152151B6F42C1D61FE4139D34B424C8647D183142ECF
+   * C1831F6E979C6DA907E88B1CAD602DB59E2F");
    */
   async signTransaction(
       path: string,
       rawTxHex: string,
       ed25519?: boolean
   ): Promise<string> {
-    const bipPath = BIPPath.fromString(path).toPathArray();
-    const rawTx = Buffer.from(rawTxHex, "hex");
-    const curveMask = ed25519 ? 0x80 : 0x40;
+    const bipPath = BIPPath.fromString(path).toPathArray()
+    const rawTx = Buffer.from(rawTxHex, 'hex')
+    const curveMask = ed25519 ? 0x80 : 0x40
 
-    const apdus = [];
-    let offset = 0;
-
+    const apdus = []
+    let offset = 0
     while (offset !== rawTx.length) {
-      const isFirst = offset === 0;
-      const maxChunkSize = isFirst ? 150 - 1 - bipPath.length * 4 : 150;
+      const isFirst = offset === 0
+      const maxChunkSize = isFirst ? 150 - 1 - bipPath.length * 4 : 150
 
-      const hasMore = offset + maxChunkSize < rawTx.length;
-      const chunkSize = hasMore ? maxChunkSize : rawTx.length - offset;
+      const hasMore = offset + maxChunkSize < rawTx.length
+      const chunkSize = hasMore ? maxChunkSize : rawTx.length - offset
 
       const apdu = {
         cla: 0xe0,
@@ -126,41 +131,41 @@ export default class CSC {
         p2: curveMask,
         data: isFirst
             ? Buffer.alloc(1 + bipPath.length * 4 + chunkSize)
-            : Buffer.alloc(chunkSize),
-      };
+            : Buffer.alloc(chunkSize)
+      }
 
       if (isFirst) {
-        apdu.data.writeInt8(bipPath.length, 0);
+        apdu.data.writeInt8(bipPath.length, 0)
         bipPath.forEach((segment, index) => {
-          apdu.data.writeUInt32BE(segment, 1 + index * 4);
-        });
+          apdu.data.writeUInt32BE(segment, 1 + index * 4)
+        })
         rawTx.copy(
             apdu.data,
             1 + bipPath.length * 4,
             offset,
             offset + chunkSize
-        );
+        )
       } else {
-        rawTx.copy(apdu.data, 0, offset, offset + chunkSize);
+        rawTx.copy(apdu.data, 0, offset, offset + chunkSize)
       }
 
-      apdus.push(apdu);
-      offset += chunkSize;
+      apdus.push(apdu)
+      offset += chunkSize
     }
 
-    let response = Buffer.alloc(0);
-    for (let apdu of apdus) {
+    let response = Buffer.alloc(0)
+    for (const apdu of apdus) {
       response = await this.transport.send(
           apdu.cla,
           apdu.ins,
           apdu.p1,
           apdu.p2,
           apdu.data
-      );
+      )
     }
 
     // the last 2 bytes are status code from the hardware
-    return response.slice(0, response.length - 2).toString("hex");
+    return response.slice(0, response.length - 2).toString('hex')
   }
 
   /**
